9faa685f0766e5239e3354e7e9f64bd984c6681a
tonytor <teme4842@gmail.com>
2024-06-15 15:43:03 +0300

    modified clerk layout

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,10 @@
+ const ClerkLayout = ({children}:{children:React.ReactNode})=>{
+    return (
+   
+           <main className=" h-full flex flex-col  items-center">
+           {children}
+           </main>
+    )
+}
+
+export default ClerkLayout
--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -5,21 +5,9 @@ import React from "react";
 export default function layout({children}:{children:React.ReactNode}){
     return(
         <ClerkProvider>
-             <html lang="en">
-        <body>
-          <header>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
-          <main>
+             
             {children}
-          </main>
-        </body>
-      </html>
+      
         </ClerkProvider>
     )
 }
